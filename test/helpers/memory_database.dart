227f04e666e1707fa486de2a785c73a467b25378
ViniciusVezono <vivivezono@gmail.com>
2025-09-03 12:38:04 -0300

    fix: correction medication test

--- test/helpers/memory_database.dart
+++ test/helpers/memory_database.dart
@@ -12,11 +12,18 @@ Future<Database> createTestDb() async {
 
   // Cria a tabela medications
   await db.execute('''
-    CREATE TABLE medications (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL
-    )
-  ''');
+        CREATE TABLE medications (
+          "id"	INTEGER NOT NULL,
+          "name"	TEXT NOT NULL,
+          "type"	TEXT NOT NULL,
+          "frequency_type"	TEXT NOT NULL,
+          "frequency_value" NUMERIC NOT NULL,
+          "duration"	NUMERIC NOT NULL,
+          "quantity"	NUMERIC NOT NULL,
+          "first_medication"	TEXT NOT NULL,
+          PRIMARY KEY("id" AUTOINCREMENT)
+        )
+      ''');
 
   await db.execute('''
     CREATE TABLE users (
