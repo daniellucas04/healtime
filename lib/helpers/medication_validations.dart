199b0f929fa3dd417ab04208ab5912c4f088657d
unknown <daniellsilva@outlook.com>
2025-09-17 21:05:37 -0300

    feat: added update and delete methods and validations for medications

--- lib/helpers/medication_validations.dart
+++ lib/helpers/medication_validations.dart
@@ -6,6 +6,34 @@ class MedicationValidations {
   MedicationValidations({required this.medication});
 
   bool validate() {
+    if (medication.name == '') {
+      return false;
+    }
+
+    if (medication.type == '') {
+      return false;
+    }
+
+    if (medication.frequencyType == '') {
+      return false;
+    }
+
+    if (medication.frequencyValue <= 0) {
+      return false;
+    }
+
+    if (medication.duration <= 0) {
+      return false;
+    }
+
+    if (medication.quantity <= 0) {
+      return false;
+    }
+
+    if (medication.firstMedication == '') {
+      return false;
+    }
+
     return true;
   }
 }
