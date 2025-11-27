fd1bc1e1430cc5a4a11a8ebdfddf3ae0c39e59be
unknown <daniellsilva@outlook.com>
2025-11-26 21:33:14 -0300

    refactor: changes to upgrade project version

--- lib/views/medicine/create_medication_step1_name.dart
+++ lib/views/medicine/create_medication_step1_name.dart
@@ -3,14 +3,50 @@ import 'package:app/views/components/header.dart';
 import 'package:app/views/components/form_input.dart';
 import 'package:app/views/medicine/create_medication_step2_type.dart';
 import 'package:app/views/theme/theme.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class CreateMedicationStep1Name extends StatelessWidget {
+class CreateMedicationStep1Name extends StatefulWidget {
   CreateMedicationStep1Name({
     super.key,
   });
 
+  @override
+  State<CreateMedicationStep1Name> createState() =>
+      _CreateMedicationStep1NameState();
+}
+
+class _CreateMedicationStep1NameState extends State<CreateMedicationStep1Name> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
   final TextEditingController medicationName = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _initCamera();
+  }
+
+  _initCamera() async {
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+
+    _controller = CameraController(
+      firstCamera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +78,44 @@ class CreateMedicationStep1Name extends StatelessWidget {
                         label: 'Nome',
                       ),
                     ],
+                  ),
+                  SizedBox(
+                    height: context.heightPercentage(0.06),
+                  ),
+                  FutureBuilder<void>(
+                    future: _initializeControllerFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return Expanded(
+                          child: CameraPreview(_controller),
+                        );
+                      } else {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  ),
+                  FloatingActionButton(
+                    // Provide an onPressed callback.
+                    onPressed: () async {
+                      // Take the Picture in a try / catch block. If anything goes wrong,
+                      // catch the error.
+                      try {
+                        // Ensure that the camera is initialized.
+                        await _initializeControllerFuture;
+
+                        // Attempt to take a picture and then get the location
+                        // where the image file is saved.
+                        final image = await _controller.takePicture();
+                        print(image.path);
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                        print(e);
+                      }
+                    },
+                    child: const Icon(Icons.camera_alt),
+                  ),
+                  SizedBox(
+                    height: context.heightPercentage(0.06),
                   ),
                   SizedBox(
                     height: 45,
